8acf9d024cb9265ac318749b68a1c77195aa6b3d
Veronica Rojas <veronicarojasmora04@gmail.com>
2026-04-29 02:32:59 -0400

    "improved documentation and tests indentation"

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,20 +1,20 @@
 import sys
 import os
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from logic_utils import check_guess
 
-def test_winning_guess():
-    # If the secret is 50 and guess is 50, it should be a win
-    outcome, _ = check_guess(50, 50)
+
+def test_win():
+    outcome, msg = check_guess(5, 5)
     assert outcome == "Win"
 
-def test_guess_too_high():
-    # If secret is 50 and guess is 60, hint should be "Too High"
-    outcome, _ = check_guess(60, 50)
+
+def test_too_high():
+    outcome, msg = check_guess(10, 5)
     assert outcome == "Too High"
 
-def test_guess_too_low():
-    # If secret is 50 and guess is 40, hint should be "Too Low"
-    outcome, _ = check_guess(40, 50)
+
+def test_too_low():
+    outcome, msg = check_guess(2, 5)
     assert outcome == "Too Low"
